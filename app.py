6eaec5d5977236e2d1ccfc6f33dbf1e821c20cd4
Seniha <seniha.uni@gmail.com>
2026-03-24 15:57:07 +0100

    Import mul() and handle choice 3

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ def run():
     choice = input("Select: ")
     if choice== "1": print(add(a, b))
     if choice == "2":print(sub(a, b))
+    if choice == "3":print(mul(a, b))
     a= float(input("a: "))
     b = float(input("b: "))
